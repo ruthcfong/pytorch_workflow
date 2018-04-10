80c53b2e8f0730403eb6418b1b75742e64dfcfde
Ruth Fong <ruthcfong@gmail.com>
2018-04-10 13:03:22 +0100

    new script

---
+++ scripts/download_and_prepare_cars.sh
@@ -0,0 +1,27 @@
+!/bin/bash
+
+DOWNLOAD_DIR=/home/ruthfong/datasets/cars
+
+# make directory (and intermediary directories) if it doesn't exist
+mkdir -p $DOWNLOAD_DIR
+cd $DOWNLOAD_DIR
+
+# download training images
+wget http://imagenet.stanford.edu/internal/car196/cars_train.tgz
+tar -xvf cars_train.tgz
+rm cars_train.tgz
+
+# downlaod test images
+wget http://imagenet.stanford.edu/internal/car196/cars_test.tgz
+tar -xvf cars_test.tgz
+rm car_test.tgz
+
+# download dev kit
+wget https://ai.stanford.edu/~jkrause/cars/car_devkit.tgz
+tar -xvf car_devkit.tgz
+rm car_devkit.tgz
+
+# download BMW10 dataset
+wget http://imagenet.stanford.edu/internal/car196/bmw10_release.tgz
+tar -xvf bmw10_release.tgz
+rm bmw10_release.tgz
