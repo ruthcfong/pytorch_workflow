b4c9f80ebc81406197d3aa4cd11268d34e2fd042
Ruth Fong <ruthfong@robots.ox.ac.uk>
2019-11-29 12:58:32 +0000

    update

--- scripts/copy_imagenet.sh
+++ scripts/copy_imagenet.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-SOURCE_DIR="/scratch/shared/nfs1/ruthfong/imagenet12"
+SOURCE_DIR="/scratch/shared/nfs1/ruthfong/ILSVRC2012/images"
 NEW_DIR="$TMPDIR/imagenet12"
 
-rsync -avz $SOURCE_DIR $NEW_DIR
+mkdir $NEW_DIR
+rsync -avz "$SOURCE_DIR/train" "$NEW_DIR/train"
+rsync -avz "$SOURCE_DIR/val_pytorch" "$NEW_DIR/val"
